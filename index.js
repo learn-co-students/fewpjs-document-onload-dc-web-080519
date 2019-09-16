9747a7d03fe1949d15357c769f300fcfb4b9965d
github username <github email address>
2019-09-16 18:29:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+let wait = document.querySelector("#text")
+document.addEventListener("DOMContentLoaded", function() {
+  wait.innerHTML = `<p id="text">This is really cool!</p>`;
+  console.log("The DOM has loaded")
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+//
+// <p id="text">JavaScript is so cool. It lets me add text to my page programmatically.</p>
+//
+// <p id="text">JavaScript is so cool. It lets me add text to my page programmatically.</p>
